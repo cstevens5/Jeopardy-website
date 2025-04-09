4eeb49799cb88af5968af6a7473d9465d07af56a
Charlie Stevens <charliestevens@Charlies-MacBook-Pro-4.local>
2025-04-09 11:32:34 -0400

    fixed bug in the clue route

--- backend/routes/clueRoute.js
+++ backend/routes/clueRoute.js
@@ -28,9 +28,16 @@ router.get("/game/:id", async (req, res) => {
 
 // get grouped clues by round
 router.get("/grouped/:game_id/:round", async (req, res) => {
+  const { game_id, round } = req.params;
+  if (isNaN(game_id)) {
+    return res.status(400).json({ error: "Invalid game id" });
+  } else if (round !== "J!" && round !== "DJ!") {
+    return res.status(400).json({ error: "Invalid round parameter" });
+  }
+
   try {
-    const clues = await Clue.find({ game_id: req.params.game_id });
-    const grouped = groupCluesForGame(clues, req.params.round);
+    const clues = await Clue.find({ game_id: game_id });
+    const grouped = groupCluesForGame(clues, round);
     res.status(200).json(grouped);
   } catch (err) {
     console.error(err);
