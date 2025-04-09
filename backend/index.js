31c3dce35fca304aed497d82d31ae6841e62f399
Charlie Stevens <charliestevens@Charlies-MacBook-Pro-4.local>
2025-04-09 13:48:59 -0400

    added some routes for game objects

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import clueRoute from "./routes/clueRoute.js";
+import gameRoute from "./routes/gameRoute.js";
 
 const app = express();
 dotenv.config();
@@ -17,6 +18,7 @@ mongoose
 // routes
 app.use(express.json());
 app.use("/api/clues", clueRoute);
+app.use("/api/games", gameRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is Running on Port:", process.env.PORT);
