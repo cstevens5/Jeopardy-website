d9ac1a3dafbad5b1f6b0536d09f91894b6e93ec6
Charlie Stevens <charliestevens@Charlies-MacBook-Pro-4.local>
2025-04-10 13:07:56 -0400

    fixed random game error

--- backend/routes/gameRoute.js
+++ backend/routes/gameRoute.js
@@ -5,31 +5,6 @@ import { groupCluesForGame } from "../groupClues.js";
 
 const router = express.Router();
 
-// route to get the game id given the air date
-router.get("/:airdate", async (req, res) => {
-  const { airdate } = req.params;
-
-  // Create range for the day
-  const start = new Date(airdate);
-  const end = new Date(airdate);
-  end.setDate(end.getDate() + 1);
-
-  try {
-    const game = await Game.findOne({
-      air_date: { $gte: start, $lt: end },
-    });
-
-    if (!game) {
-      return res.status(404).json({ error: "Game not found for this airdate" });
-    }
-
-    res.status(200).json(game.id);
-  } catch (err) {
-    console.log("Error fetching game id: ", err);
-    res.status(500).json({ error: err });
-  }
-});
-
 // route to get a list of grouped clues given the air date of the episode
 router.get("/grouped/:airdate/:round", async (req, res) => {
   const { airdate, round } = req.params;
@@ -68,13 +43,13 @@ router.get("/grouped/:airdate/:round", async (req, res) => {
 // route to get a random game
 router.get("/random", async (req, res) => {
   try {
-    const [random_game] = await Game.aggregate([{ $sample: { size: 1 } }]);
+    const random_game = await Game.aggregate([{ $sample: { size: 1 } }]);
 
     if (!random_game) {
       return res.status(404).json({ error: "No games found" });
     }
 
-    res.status(200).json({ game_id: random_game.id });
+    res.status(200).json({ game_id: random_game[0].id });
   } catch (err) {
     console.error("Error selecting random game:", err);
     res.status(500).json({ error: "Server error" });
